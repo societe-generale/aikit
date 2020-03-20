6fbe28f401a5f16697024bd8bfa848950a79fdde
LionelMassoulard <lionel.massoulard@gmail.com>
2020-03-20 15:11:14 +0100

    add 'OrdinalEncoderV2' that also works for non sorted categories

--- aikit/dev_classifier_to_regressor.py
+++ aikit/dev_classifier_to_regressor.py
@@ -49,7 +49,7 @@ class RegressorFromClassifier(BaseEstimator, RegressorMixin):
             y_clusterer = self.get_default_y_cluster(y)
         else:
             y_clusterer = self.y_clusterer
-            # TODO : check that it is a cluserer
+            # TODO : check that it is a clusterer
             
         if not is_classifier(self.classifier_model):
             raise TypeError("classifier_model should be a classifer")
@@ -93,18 +93,123 @@ class RegressorFromClassifier(BaseEstimator, RegressorMixin):
 
 from sklearn.preprocessing import OrdinalEncoder
 
-OrdinalEncoder(dtype=np.int32).fit_transform(y_ord[:,np.newaxis])
-OrdinalEncoder(dtype=np.int32).fit_transform(y_ord[:,np.newaxis].astype(str))
+
+
+
+# In[]
+from sklearn.base import TransformerMixin
+
+from sklearn.utils.validation import check_is_fitted
+from aikit.tools.data_structure_helper import convert_generic, DataTypes
+
+class OrdinalEncoderV2(BaseEstimator, TransformerMixin):
+    
+    def __init__(self,
+                 categories="auto",
+                 dtype=np.int32
+                 ):
+        self.categories=categories
+        self.dtype=dtype
+                
+    def fit(self, X, y=None):
+
+        X = convert_generic(X, output_type=DataTypes.NumpyArray)
+        if X.ndim != 2:
+            raise TypeError("This transformer expect a two dimensional array")
+            
+        self._nb_columns = X.shape[1]
+            
+        is_auto = isinstance(self.categories, str) and self.categories == "auto"
+        
+        if not is_auto:
+            if len(self.categories) != X.shape[1]:
+                raise TypeError("categories should be 'auto' or a list the same size as 'X.shape[1]'")
+
+        all_mappings = []
+        all_inv_mappings = []
+        categories = []
+        for j in range(X.shape[1]):
+            
+            if is_auto:
+                target_classes_j = np.sort(np.unique(X[:, j]))
+            else:
+                target_classes_j = np.array(self.categories[j])
+                uy_j = set(list(X[:, j]))
+                if len(set(list(uy_j)).difference(target_classes_j)) > 0:
+                    raise ValueError("I have a categories that doesn't exist, please chekc")
+
+            integers = np.arange(len(target_classes_j)).astype(self.dtype)
+            mapping = pd.Series(integers, index = target_classes_j)
+            inv_mapping = pd.Series(target_classes_j, index = integers)
+            
+            all_mappings.append(mapping)
+            all_inv_mappings.append(inv_mapping)
+            categories.append(target_classes_j)
+        
+        self.categories_ = categories
+        self._all_mapping = all_mappings
+        self._all_inv_mapping = all_inv_mappings
+        
+        return self
+    
+
+    def transform(self, X):
+        check_is_fitted(self)
+        
+        X = convert_generic(X, output_type=DataTypes.NumpyArray)
+
+        if X.ndim != 2:
+            raise TypeError("This transformer expect a two dimensional array")
+            
+        if X.shape[1] != self._nb_columns:
+            raise ValueError("X doesn't have the correct number of columns")
+            
+        Xres = np.zeros(X.shape, dtype=self.dtype)
+        for j in range(X.shape[1]):
+            Xres[:, j] = self._all_mapping[j].loc[X[:, j]].values
+        
+        return Xres
+    
+    def inverse_transform(self, X):
+        check_is_fitted(self)
+        
+        X = convert_generic(X, output_type=DataTypes.NumpyArray)
+
+        if X.ndim != 2:
+            raise TypeError("This transformer expect a two dimensional array")
+            
+        if X.shape[1] != self._nb_columns:
+            raise ValueError("X doesn't have the correct number of columns")
+        
+        Xres = []
+        for j in range(X.shape[1]):
+            Xres.append(  self._all_inv_mapping[j].loc[ X[:, j]].values[:, np.newaxis] )
+            
+        return np.concatenate(Xres, axis=1)
+            
+# In[]
+OrdinalEncoderV2(dtype=np.int32).fit_transform(y_ord[:,np.newaxis])
+OrdinalEncoderV2(dtype=np.int32).fit_transform(y_ord[:,np.newaxis].astype(str))
 
 y = np.array(["z","a","b"]*3)
-enc = OrdinalEncoder(dtype=np.int32)
-enc.fit_transform(y[:, np.newaxis])[:, 0]
+enc = OrdinalEncoderV2(dtype=np.int32)
+y_enc = enc.fit_transform(y[:, np.newaxis])
+assert (enc.inverse_transform(y_enc)[:,0] == y).all()
 enc.categories_
 
 y = np.array(["z","a","b"]*3)
-enc = OrdinalEncoder(dtype=np.int32, categories=[["a","b","c","z"]])
-enc.fit_transform(y[:, np.newaxis])[:, 0]
+enc = OrdinalEncoderV2(dtype=np.int32, categories=[["a","b","c","z"]])
+
+y_enc = enc.fit_transform(y[:, np.newaxis])
+assert (enc.inverse_transform(y_enc)[:,0] == y).all()
 enc.categories_
+
+y = np.array(["z","a","b"]*3)
+enc = OrdinalEncoderV2(dtype=np.int32, categories=[["z","a","b"]])
+y_enc = enc.fit_transform(y[:, np.newaxis])
+assert (enc.inverse_transform(y_enc)[:,0] == y).all()
+enc.categories_
+
 
 # In[]
 
@@ -115,8 +220,8 @@ class ClassifierFromRegressor(BaseEstimator, ClassifierMixin):
 
     def __init__(self,
                  regressor_model,
-                 classes="auto",
-                 kernel_windows=0.2
+                 kernel_windows=0.2,
+                 classes=None
                  ):
         self.regressor_model=regressor_model
         self.classes=classes
@@ -124,22 +229,31 @@ class ClassifierFromRegressor(BaseEstimator, ClassifierMixin):
         
         
     def fit(self, X, y):
+        
+        self._mono_target = y.ndim == 1
 
         ## Conversion of target into integer      
-        self._target_encoder = OrdinalEncoder(dtype=np.int32) # TODO add classes here
+        if self.classes is None:
+            categories = "auto"
+        else:
+            if self._mono_target:
+                categories = [self.classes] # because OrdinalEncoder expect a list
+            else:
+                if not isinstance(self.classes, list):
+                    raise TypeError("For multi-target classes should be a list, instead I got %s" % str(type(self.classes)))
+                
+                categories = self.classes
+            
+        self._target_encoder = OrdinalEncoderV2(dtype=np.int32, categories=categories) # ca ne marche pas si les classes sont pas ordonnées !
         yd2 = make2dimensions(y)
         
         yd2_int = self._target_encoder.fit_transform(yd2)
         
-        if y.ndim == 1:
-            self._mono_target = True
+        if self._mono_target:
             y_int = yd2_int[:,0]
-            
             assert len(self._target_encoder.categories_) == 1
         else:
-            self._mono_target = False
-            y_int = yd2_int # I keep the multi-dimension
-            
+            y_int = yd2_int # I keep the multi-dimension            
             assert len(self._target_encoder.categories_) == y.shape[1]
         
         self.regressor_model.fit(X, y_int)
@@ -190,7 +304,7 @@ class ClassifierFromRegressor(BaseEstimator, ClassifierMixin):
             return probas
 
     def distance_to_proba(self, d):
-        e = np.exp(-d/self.kernel_windows)
+        e = np.exp(-d/self.kernel_windows) # TODO : find a good heuristic for that kernel_windows
         
         return e / e.sum(axis=1, keepdims=True)
 
@@ -217,6 +331,30 @@ assert proba.min() >= 0
 assert proba.max() <= 1
 assert not pd.isnull(proba).any()
 assert np.abs(proba.sum(axis=1) - 1).max() <= 0.0001
+
+
+X = np.random.randn(9, 2)
+y = np.array(["c","a","b"]*3)
+
+self = ClassifierFromRegressor(regressor_model=Ridge(), classes=["c","a","b"])
+self.fit(X, y)
+
+proba = self.predict_proba(X)
+
+yhat = self.predict(X)
+
+assert list(self.classes_) == ["c","a","b"]
+assert proba.shape == (y.shape[0] , 3)
+assert yhat.shape == y.shape
+assert (self.classes_[proba.argmax(axis=1)] == yhat).all()
+assert proba.min() >= 0
+assert proba.max() <= 1
+assert not pd.isnull(proba).any()
+assert np.abs(proba.sum(axis=1) - 1).max() <= 0.0001
+
+
+
+
 
 # In[]
         
